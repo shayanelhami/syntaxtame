13ea90ebcdd4e25cc89625323ff6b4fa26126b91
Shayan Elhami <shayanelhami@gmail.com>
2012-07-27 10:59:19 +0100

    Initial structure

---
+++ src/Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var highlighted = Highlighter.Code.GetHighlighted(@"
+// your code goes here
+");
+
+            File.WriteAllText(".\\index.html", highlighted);
+            Console.WriteLine("Done, open 'index.html' in borwser");
+        }
+    }
+}
---
+++ src/Highlighter/Code.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Highlighter
+{
+    public class Code
+    {
+        public static string GetHighlighted(string sourceCode)
+        {
+            // TODO:
+            throw new NotImplementedException();
+        }
+    }
+}
